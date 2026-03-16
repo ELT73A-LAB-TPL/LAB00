9b91b3901092a23b42d16c87a24d5f68284d9415
ruseleredu <ruseler@utfpr.edu.br>
2026-03-16 19:16:53 -0300

    Update check.bat to log GitHub CLI version
    Replaced MinGW tool version checks with GitHub CLI version check.

--- check.bat
+++ check.bat
@@ -1,6 +1,4 @@
 git -v >> git-config.txt
 git config list --show-origin >> git-config.txt
-gcc --version >> mingw-config.txt
-cmake --version >> mingw-config.txt
-gdb --version >> mingw-config.txt
+gh --version >> git-config.txt
 code -v >> vscode-config.txt
